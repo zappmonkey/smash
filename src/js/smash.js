57efff8b28b515745860341b75f221b0d56acc07
Mark Freese <mark@zappmonkey.com>
2017-02-06 23:50:28 +0100

    Working on layout

--- src/js/smash.js
+++ src/js/smash.js
@@ -1,1 +1,21 @@
-var smash = {};
+window.onload = function(e){
+    smash.init();
+}
+
+var smash = {
+    init: function() {
+        var sb = document.querySelector(".smash-searchbox input");
+        sb.onfocus = function() {
+            smash.addClass(document.querySelector(".smash-header"), 'is-focussed');
+        };
+        sb.onblur = function() {
+            smash.removeClass(document.querySelector(".smash-header"), 'is-focussed');
+        };
+    },
+    addClass: function(el, className) {
+        el.classList ? el.classList.add(className) : el.className += (' ' + className);
+    },
+    removeClass: function(el, className) {
+        el.classList ? el.classList.remove(className) : el.className = el.className.replace(className, '');
+    }
+};
