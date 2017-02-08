7205e8ab79435a12c3b20cf0e454613bc214ef96
Mark Freese <mark@zappmonkey.com>
2017-02-08 17:22:17 +0100

    Working on sidebar..

---
+++ src/js/modules/class.js
@@ -0,0 +1,17 @@
+smash.class = {};
+
+smash.class.add = function(el, className) {
+    el.classList ? el.classList.add(className) : el.className += (' ' + className);
+};
+
+smash.class.remove = function(el, className) {
+    el.classList ? el.classList.remove(className) : el.className = el.className.replace(className, '');
+};
+
+smash.class.has = function(el, className) {
+    return ((" " + el.className + " ").replace(/[\n\t]/g, " ").indexOf(" " + className + " ") > -1);
+};
+
+smash.class.toggle = function(el, className) {
+    smash.class.has(el, className) ? smash.class.remove(el, className) : smash.class.add(el, className);
+};
