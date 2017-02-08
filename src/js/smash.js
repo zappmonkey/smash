0f756b774b24365f8409d790aaf4f3cd79b6a0fe
Mark Freese <mark@zappmonkey.com>
2017-02-08 09:41:35 +0100

    Added lock and unlock to sidebar

--- src/js/smash.js
+++ src/js/smash.js
@@ -5,17 +5,36 @@ window.onload = function(e){
 var smash = {
     init: function() {
         var sb = document.querySelector(".smash-searchbox input");
-        sb.onfocus = function() {
-            smash.addClass(document.querySelector(".smash-header"), 'is-focussed');
-        };
-        sb.onblur = function() {
-            smash.removeClass(document.querySelector(".smash-header"), 'is-focussed');
-        };
+        if (sb) {
+            sb.onfocus = function() {
+                smash.addClass(document.querySelector(".smash-header"), 'is-focussed');
+            };
+            sb.onblur = function() {
+                smash.removeClass(document.querySelector(".smash-header"), 'is-focussed');
+            };
+        }
+        var ibl = document.querySelector(".smash-icon-bar .lock");
+        if (ibl) {
+            ibl.onclick = function() {
+                smash.toggleClass(this, 'unlock');
+                smash.toggleClass(document.querySelector(".smash-icon-bar"), 'is-unlocked');
+            };
+        }
+        var ib = document.querySelector(".smash-icon-bar");
+        if (ib) {
+            smash.addClass(ib, 'is-unlocked');
+        }
     },
     addClass: function(el, className) {
         el.classList ? el.classList.add(className) : el.className += (' ' + className);
     },
     removeClass: function(el, className) {
         el.classList ? el.classList.remove(className) : el.className = el.className.replace(className, '');
+    },
+    hasClass: function(el, className) {
+        return ((" " + el.className + " ").replace(/[\n\t]/g, " ").indexOf(" " + className + " ") > -1);
+    },
+    toggleClass: function(el, className) {
+        smash.hasClass(el, className) ? smash.removeClass(el, className) : smash.addClass(el, className);
     }
 };
