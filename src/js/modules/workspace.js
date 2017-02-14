63e1c249471861b0c353def167f6310a84bb1430
Mark Freese <mark@zappmonkey.com>
2017-02-14 23:21:40 +0100

    Working tabs

--- src/js/modules/workspace.js
+++ src/js/modules/workspace.js
@@ -32,17 +32,49 @@ smash.workspace.addTab = function(id, title, content) {
     var tabs = smash.get(document, '.smash-tab-pane .smash-tabs');
     var panesContainer = smash.get(document, '.smash-tab-pane .smash-panes');
 
+    if (tab = smash.get(tabs, '.smash-pane[data-id="' + id + '"]')) {
+        tab.onclick();
+        return;
+    }
+
     smash.hide(panesContainer, ".smash-pane");
     if (active = smash.get(tabs, '.smash-tab.active')) {
         smash.class.remove(active, 'active');
     }
-    tabs.innerHTML = tabs.innerHTML + '<div class="smash-tab active" data-id="' + id + '">' + title + '<i class="material-icons close">close</i></div>';
-    panesContainer.innerHTML = panesContainer.innerHTML + '<div class="smash-pane" data-id="' + id + '">' + content + '</div>';
-    smash.on(tabs, '.smash-tab', 'onclick', function(e) {
-        console.log('test');
+
+    // Create a new tab
+    var tab = document.createElement("div");
+    tab.className = 'smash-tab active';
+    tab.dataset.id = id;
+    tab.innerHTML = title + '<i class="material-icons close">close</i>';
+    tab.onclick = function(e) {
         if (active = smash.get(tabs, '.smash-tab.active')) {
             smash.class.remove(active, 'active');
         }
         smash.class.add(this, 'active');
+        smash.hide(document, '.smash-pane');
+        smash.show(document, '.smash-pane[data-id="' + this.dataset.id + '"]');
+    };
+    smash.on(tab, '.close', 'onclick', function(e) {
+        if (smash.class.has(this.parentElement, 'active')) {
+            if (this.parentElement.previousSibling) {
+                this.parentElement.previousSibling.onclick();
+            } else if (this.parentElement.nextSibling) {
+                this.parentElement.nextSibling.onclick();
+            }
+        }
+        if (pane = smash.get(document, '.smash-pane[data-id="' + this.parentElement.dataset.id + '"]')) {
+            pane.parentElement.removeChild(pane);
+        }
+        this.parentElement.parentElement.removeChild(this.parentElement);
+        e.stopPropagation();
     });
+    tabs.appendChild(tab);
+
+    // Create a new pane with the provided content
+    var pane = document.createElement("div");
+    pane.className = 'smash-pane';
+    pane.dataset.id = id;
+    pane.innerHTML = content;
+    panesContainer.appendChild(pane);
 };
