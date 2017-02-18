2676ea661a20433e2b21b15f8f015f1be2acffce
Mark Freese <mark@zappmonkey.com>
2017-02-18 11:32:20 +0100

    Add searchbox event functions and buttons

--- src/js/modules/searchbox.js
+++ src/js/modules/searchbox.js
@@ -1,5 +1,9 @@
 smash.searchbox = {
-    i_: false
+    i_: false,
+    minAutoCompleteLength: 3,
+    onfocus: undefined,
+    onblur: undefined,
+    onvalue: undefined,
 };
 
 smash.searchbox.init = function() {
@@ -12,9 +16,23 @@ smash.searchbox.init = function() {
     }
     ssb.onfocus = function() {
         smash.class.add(document.querySelector(".smash-header"), 'is-focussed');
+        if (smash.searchbox.onfocus) {
+            smash.searchbox.onfocus();
+        }
     };
     ssb.onblur = function() {
         smash.class.remove(document.querySelector(".smash-header"), 'is-focussed');
+        if (smash.searchbox.onblur) {
+            smash.searchbox.onblur();
+        }
+    };
+    ssb.onkeyup = function(e) {
+        if (this.value.length < smash.searchbox.minAutoCompleteLength) {
+            return;
+        }
+        if (smash.searchbox.onchange) {
+            smash.searchbox.onchange(this.value);
+        }
     };
     smash.searchbox.i_ = true;
 };
