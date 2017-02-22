966de46b0881223d2e748bff82de0d92495e3259
Mark Freese <mark@zappmonkey.com>
2017-02-22 13:38:49 +0100

    Added set/get value methods on input en select

--- src/js/modules/input.js
+++ src/js/modules/input.js
@@ -1,30 +1,48 @@
 smash.input = {};
 
 smash.input.init = function() {
-    var inputs = document.querySelectorAll(".smash-input.has-floating-label input");
+    var inputs = document.querySelectorAll(".smash-input input");
     var input;
     for (var i = 0; i < inputs.length; i++) {
         input = inputs[i];
         if (smash.class.has(input.parentElement, 'smash-initialised')) {
             continue;
         }
-        smash.class.remove(input.parentElement, 'has-floating-label');
-        smash.class.add(input.parentElement, 'floating-label');
 
-        var label = document.createElement('label');
-        label.innerHTML = input.placeholder;
-        input.parentElement.appendChild(label);
-        input.onkeyup = function(e) {
-            var l = this.parentElement.querySelector("label");
-            if (l) {
-                if (this.value != "") {
-                    smash.class.add(l, 'show');
-                } else {
-                    smash.class.remove(l, 'show');
-                }
-            }
+        input.parentElement.setValue = function(value) {
+            var input = this.querySelector("input");
+            input.value = value;
+            input.onkeyup();
         };
-        input.onkeyup();
+
+        input.parentElement.getValue = function() {
+            var input = this.querySelector("input");
+            return input.value
+        };
+
+        if (smash.class.has(input.parentElement, 'has-floating-label')) {
+            smash.class.remove(input.parentElement, 'has-floating-label');
+            smash.class.add(input.parentElement, 'floating-label');
+
+            var label = document.createElement('label');
+            label.innerHTML = input.placeholder;
+            input.parentElement.appendChild(label);
+            input.onkeyup = function(e) {
+                var l = this.parentElement.querySelector("label");
+                if (l) {
+                    if (this.value != "") {
+                        smash.class.add(l, 'show');
+                    } else {
+                        smash.class.remove(l, 'show');
+                    }
+                }
+            };
+        }
+
+        if (input.parentElement.getAttribute('value')) {
+            input.parentElement.setValue(input.parentElement.getAttribute('value'));
+        }
+
         smash.class.add(input.parentElement, 'smash-initialised');
     }
     var selects = document.querySelectorAll(".smash-select select");
@@ -41,6 +59,25 @@ smash.input.init = function() {
             label.innerHTML = select.getAttribute('placeholder');
             select.parentElement.appendChild(label);
         }
+
+        select.parentElement.setValue = function(value) {
+            smash.class.remove(this, 'is-focussed');
+            var select = this.querySelector("select");
+            var input = this.querySelector("input");
+            select.value = value;
+            input.value = select.options[select.selectedIndex].text;
+            input.focus();
+            input.onfocus();
+        };
+
+        select.parentElement.getValue = function() {
+            var select = this.querySelector("select");
+            if (select.selectedIndex == -1) {
+                return null;
+            }
+            return select.options[select.selectedIndex].value;
+        };
+
         var input = document.createElement('input');
         input.type = "text";
         input.placeholder = select.getAttribute('placeholder');
@@ -51,22 +88,17 @@ smash.input.init = function() {
         var ul = document.createElement('ul');
         for (var j = 0; j < options.length; j++) {
             var option = options[j];
-            if (option.getAttribute('value')) {
-                var li = document.createElement('li');
-                li.setAttribute("value", option.value);
-                li.innerHTML = option.innerHTML;
-                ul.appendChild(li);
-                li.onmousedown = function(e) {
-                    var val = this.getAttribute('value');
-                    var parent = this.parentElement.parentElement;
-                    smash.class.remove(parent, 'is-focussed');
-                    window.onmousedown = null;
-                    parent.querySelector("select").value = val;
-                    parent.querySelector("input").value = this.innerHTML;
-                    parent.querySelector("input").focus();
-                    parent.querySelector("input").onfocus();
-                    e.stopPropagation();
-                }
+            if (!option.getAttribute('value')) {
+                continue;
+            }
+            var li = document.createElement('li');
+            li.setAttribute("value", option.value);
+            li.innerHTML = option.innerHTML;
+            ul.appendChild(li);
+            li.onmousedown = function(e) {
+                this.parentElement.parentElement.setValue(this.getAttribute('value'));
+                e.stopPropagation();
+                window.onmousedown = null;
             }
         }
         select.parentElement.appendChild(ul);
@@ -89,6 +121,10 @@ smash.input.init = function() {
                 smash.class.remove(select, 'is-focussed');
             };
         };
+
+        if (select.parentElement.getAttribute('value')) {
+            select.parentElement.setValue(select.parentElement.getAttribute('value'));
+        }
         smash.class.add(select.parentElement, 'smash-initialised');
     }
 };
