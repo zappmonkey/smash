d15ec2d3b6a2ff6e1d5113404734ccce476a6f96
Mark Freese <mark@zappmonkey.com>
2017-08-22 22:10:28 +0200

    Get all query params from url

--- src/js/modules/router.js
+++ src/js/modules/router.js
@@ -218,3 +218,67 @@ smash.router.queryParam = function(param) {
     }
     return results[1];//decodeURIComponent(results[1].replace(/\+/g, " "));
 };
+
+/**
+ * Get the query param in the url
+ * @param  string param The param you want to have
+ * @return string       The value of the param or undefined if it is not available
+ */
+smash.router.queryParams = function(url) {
+
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+    // we'll store the parameters here
+    var obj = {};
+
+    // if query string exists
+    if (queryString) {
+
+        // stuff after # is not part of query string, so get rid of it
+        queryString = queryString.split('#')[0];
+
+        // split our query string into its component parts
+        var arr = queryString.split('&');
+
+        for (var i=0; i<arr.length; i++) {
+            // separate the keys and the values
+            var a = arr[i].split('=');
+
+            // in case params look like: list[]=thing1&list[]=thing2
+            var paramNum = undefined;
+            var paramName = a[0].replace(/\[\d*\]/, function(v) {
+                paramNum = v.slice(1,-1);
+                return '';
+            });
+
+            // set parameter value (use 'true' if empty)
+            var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+
+            // (optional) keep case consistent
+            paramName = paramName.toLowerCase();
+            paramValue = paramValue.toLowerCase();
+
+            // if parameter name already exists
+            if (obj[paramName]) {
+                // convert value to array (if still string)
+                if (typeof obj[paramName] === 'string') {
+                    obj[paramName] = [obj[paramName]];
+                }
+                // if no array index number specified...
+                if (typeof paramNum === 'undefined') {
+                    // put the value on the end of the array
+                    obj[paramName].push(paramValue);
+                } else {
+                    // if array index number specified...
+                    // put the value at that index number
+                    obj[paramName][paramNum] = paramValue;
+                }
+            // if param name doesn't exist yet, set it
+            } else {
+                obj[paramName] = paramValue;
+            }
+        }
+    }
+    return obj;
+};
